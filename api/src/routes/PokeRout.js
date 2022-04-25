d430fafdc2ebab0de70d5ea9dc4d4a1297d930f6
SHRicard <SH_Ricardo@hotmail.com>
2022-04-25 19:19:59 -0300

    ruta pedido api

--- api/src/routes/PokeRout.js
+++ api/src/routes/PokeRout.js
@@ -1,22 +1,39 @@
 const { Router } = require("express");
+const axios = require("axios");
 const { Pokemon, Tipos } = require("../db");
+const { URL_API } = process.env;
+
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
 const router = Router();
 //Lleno mi base de dato
-router.post("/", (req, res, next) => {
-  // let pokeUploadData = "Pokedex Llena"
-  // const pokeExisting = Pokemon.findAll();
-  // if(!pokeExisting.length){
-  // }
+router.post("/", async (req, res, next) => {
+  const Exists = "Los Pokemones ya Estas Registrados";
+  const pokeExists = await Pokemon.findAll();
+  if (!pokeExists.length) {
+    const urlApiData = await axios.get(URL_API);
+    const urlApiDataTwo = urlApiData.data.results.map((pokeUrl) => pokeUrl.url);
+
+    // const pokeInf = await Promise.all(
+    //   urlApiDataTwo.map((pokeInfo) => axios.get(pokeInfo))
+    // );
+
+    const fullDataPokemones = await Promise.all(
+      urlApiDataTwo.map((poke) => axios.get(poke)) //aplico el meto promis.all para que se aga al mismo tiempo
+    );
+
+    console.log(fullDataPokemones);
+    console.log(pokeInf);
+    res.send(pokeInf);
+  }
 });
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
 
 router.get("/", (req, res, next) => {
-  res.send("soy get /PokeRout");
+  res.send("soy get Ricardo");
 });
 router.post("/", (req, res, next) => {
   res.send("soy post /PokeRout");
