a822f495519651126beb46edd17527cdd489c1d3
SHRicard <SH_Ricardo@hotmail.com>
2022-04-26 20:51:29 -0300

    ruta rota

--- api/src/routes/PokeRout.js
+++ api/src/routes/PokeRout.js
@@ -12,20 +12,41 @@ router.post("/", async (req, res, next) => {
   const Exists = "Los Pokemones ya Estas Registrados";
   const pokeExists = await Pokemon.findAll();
   if (!pokeExists.length) {
-    const urlApiData = await axios.get(URL_API);
-    const urlApiDataTwo = urlApiData.data.results.map((pokeUrl) => pokeUrl.url);
+    const urlApiData = await axios.get(URL_API); // mi primer llamado a la api resultado en urlApiData
+    const urlApiDataTwo = urlApiData.data.results.map((pokeUrl) => pokeUrl.url); // segunda Url
 
-    // const pokeInf = await Promise.all(
-    //   urlApiDataTwo.map((pokeInfo) => axios.get(pokeInfo))
-    // );
-
-    const fullDataPokemones = await Promise.all(
-      urlApiDataTwo.map((poke) => axios.get(poke)) //aplico el meto promis.all para que se aga al mismo tiempo
+    const pokeInf = await Promise.all(
+      urlApiDataTwo.map((pokeInfo) => axios.get(pokeInfo))
     );
 
-    console.log(fullDataPokemones);
-    console.log(pokeInf);
-    res.send(pokeInf);
+    let pokeMaps = pokeInf.map((poke) => {
+      let pokeTipos = poke.data.types.map((types) => types.type.name); // tambien los tipo
+      let obj = {
+        id: poke.data.id,
+        name: poke.data.name,
+        life: poke.data.stats[0].base_stat,
+        atack: poke.data.stats[1].base_stat,
+        defensa: poke.data.stats[2].base_stat,
+        speed: poke.data.stats[5].base_stat,
+        height: poke.data.height,
+        weight: poke.data.weight,
+        img: poke.data.sprites.other["official-artwork"].front_default,
+        imgFront:
+          poke.data.sprites.versions["generation-v"]["black-white"].animated
+            .front_default, //fui mas adentro en la api para conseguir el git y no una imagen
+        imgFrontShiny:
+          poke.data.sprites.versions["generation-v"]["black-white"].animated
+            .front_shiny,
+        icons:
+          poke.data.sprites.versions["generation-viii"].icons.front_default, //fui mas adentro en la api para conseguir el git y no una imagen
+
+        //fui mas adentro en la api para conseguir el git y no una imagen
+        tipo: pokeTipos,
+      };
+      return obj;
+    });
+    console.log(pokeMaps);
+    return pokeMaps;
   }
 });
 
