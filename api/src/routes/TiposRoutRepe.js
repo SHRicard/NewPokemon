c910ceb03f94fe62d9d8a351ebec7f143e558041
SHRicard <SH_Ricardo@hotmail.com>
2022-04-27 22:33:09 -0300

    ruta

---
+++ api/src/routes/TiposRoutRepe.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+// Importar todos los routers;
+// Ejemplo: const authRouter = require('./auth.js');
+
+const router = Router();
+
+// Configurar los routers
+// Ejemplo: router.use('/auth', authRouter);
+
+router.get("/", (req, res, next) => {
+  res.send("soy get /TiposRout");
+});
+router.post("/", (req, res, next) => {
+  res.send("soy post /TiposRout");
+});
+router.put("/", (req, res, next) => {
+  res.send("soy put /TiposRout");
+});
+router.delete("/", (req, res, next) => {
+  res.send("soy delete /TiposRout");
+});
+module.exports = router;
